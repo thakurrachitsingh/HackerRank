468ca594512c04601cb90296796f695beca1f5f6
thakurrachitsingh <59912926+thakurrachitsingh@users.noreply.github.com>
2020-11-11 21:47:49 +0530

    program to add elements of Array created

---
+++ Problem Solving/Algorithms/Simple Array Sum/solution.java
@@ -0,0 +1,25 @@
+
+// Sum of elements of Array in Java
+
+import java.io.*;
+import java.math.*;
+import java.text.*;
+import java.util.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        int sum=0;
+        int[] arr = new int[num];
+        for(int i = 0; i<num;i++){
+            arr[i]=sc.nextInt();
+        }
+        for(int j:arr){
+            sum+=j;
+        }
+        System.out.println(sum);
+
+    }
+}
