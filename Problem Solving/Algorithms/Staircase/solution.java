642f486e06e24aa98c0b1e6bd29a249dd43a3c6e
thakurrachitsingh <59912926+thakurrachitsingh@users.noreply.github.com>
2020-11-12 21:01:17 +0530

    Program to print pattern is created

---
+++ Problem Solving/Algorithms/Staircase/solution.java
@@ -0,0 +1,34 @@
+// program to print a pattern like this according to the given integer 4
+
+
+//   #
+//  ##
+// ###
+//####
+
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= n; j++) {
+                if ((i + j) > n) {
+                    System.out.print("#");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
