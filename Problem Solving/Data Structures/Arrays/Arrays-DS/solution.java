abd3c7ec9740375d26d98d3e107fd5407f31b941
thakurrachitsingh <59912926+thakurrachitsingh@users.noreply.github.com>
2020-11-22 10:56:45 +0530

    Arrays-DS created.

---
+++ Problem Solving/Data Structures/Arrays/Arrays-DS/solution.java
@@ -0,0 +1,26 @@
+//Question-- https://www.hackerrank.com/challenges/arrays-ds/problem
+
+//Arrays-DS created.
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for(int i=(n-1);i>=0;i--){
+            arr[i] = sc.nextInt();
+        }
+        for(int a: arr){
+            System.out.print(a+" ");
+        }
+        
+    }
+}
